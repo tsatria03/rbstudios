9bbd914d169b500ae95910bc93700ade7cab1e7f
Tony Satriano <tonysatria168@gmail.com>
2025-04-23 13:32:11 -0700

    Updated the guess the number game to point to the newly created github release version of it.

--- scripts/index.js
+++ scripts/index.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const selected = comboBox.value;
       if (selected === '1') {
         heading2.textContent = 'Guess the number';
-        paragraph.innerHTML = 'Every game creator needs to start somewhere, right? Enjoy a simple guess the number game! <a href="https://www.dropbox.com/scl/fi/3yuxm9yxdbk493t8xe5lh/guesnumjs.rar?rlkey=ttnulzurn8z14lhyfvzuxbwv7&dl=1">Download it here</a>';
+        paragraph.innerHTML = 'Every game creator needs to start somewhere, right? Enjoy a simple guess the number game! <a href="https://github.com/tsatria03/guessnum_js/releases/download/V1.00/guessnumjs.rar">Download it here</a>';
       } else {
         heading2.textContent = 'Welcome to our games page!';
         paragraph.textContent = 'We currently only have one game to download, but no worries! We will create more content soon!';
