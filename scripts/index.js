3eb5ae89894c9607107d0cfaf39ced10bd626ce7
Tony Satriano <tonysatria168@gmail.com>
2025-04-23 18:26:00 -0700

    Removed non existent links from all of the navigation elements.

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const selected = comboBox.value;
       if (selected === '1') {
         heading2.textContent = 'Guess the number';
-        paragraph.innerHTML = 'Every game creator needs to start somewhere, right? Enjoy a simple guess the number game! <a href="https://github.com/tsatria03/guessnum_js/releases/download/V1.00/guessnumjs.rar">Download it here</a>';
+        paragraph.innerHTML = 'Every game creator needs to start somewhere, right? Enjoy a simple guess the number game!<br> <a href="https://github.com/tsatria03/guessnum_js/releases/download/V1.00/guessnumjs.rar">Download it here</a>';
       } else {
         heading2.textContent = 'Welcome to our games page!';
         paragraph.textContent = 'We currently only have one game to download, but no worries! We will create more content soon!';
